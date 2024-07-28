5d8ca4ab85952e88dc6ca073f59ce5d639d66dbd
Семён Войтов <semenvojtov@MacBook-Air-Semen.local>
2024-07-28 08:36:05 +0300

    v1 only back

---
+++ backend/users/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name',)
+    list_filter = ('username', 'email',)
+
+
+admin.site.register(User, UserAdmin)
